1a0e5552ad406ce3e52ce8553ce8b26cd77da158
Arnaud LOKONON <fiflokonon@gmail.com>
2024-05-30 16:18:49 +0100

    Good

--- resources/views/dashboard/client/brands.blade.php
+++ resources/views/dashboard/client/brands.blade.php
@@ -12,7 +12,7 @@
                      data-kt-swapper-parent="{default: '#kt_content_container', 'lg': '#kt_toolbar_container'}"
                      class="page-title d-flex align-items-center flex-wrap me-3 mb-5 mb-lg-0">
                     <!--begin::Title-->
-                    <h1 class="d-flex text-dark fw-bolder fs-3 align-items-center my-1">Dashboard</h1>
+                    <h1 class="d-flex text-dark fw-bolder fs-3 align-items-center my-1">CALCULA TU SEGURO DE FORMA RÁPIDA Y SENCILLA</h1>
                     <!--end::Title-->
                 </div>
                 <!--end::Page title-->
